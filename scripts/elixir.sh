703dd92d328853cd0b2682fdf92da22c73f6afeb
Matt Piercy <mpiercy@gmail.com>
2021-09-28 21:49:32 -0700

    add kiex setup to elixir script

--- scripts/elixir.sh
+++ scripts/elixir.sh
@@ -6,5 +6,13 @@ if ! [ -x "$(command -v elixir)" ]; then
 else
   echo "Elixir already installed"
 fi
+
+if ! [ -x "$(command -v kiex)" ]; then
+  \curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
+  echo 'test -s "$HOME/.kiex/scripts/kiex" && source "$HOME/.kiex/scripts/kiex"' >> ~/.zshrc
+else
+  echo "kiex already installed"
+fi
+
 echo "...done"
 echo
